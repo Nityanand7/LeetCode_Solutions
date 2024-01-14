001508984563e37264506ead30ce86d8ad03bbce
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2024-01-14 08:49:33 -0800

    Update LeetCode206.py

--- LeetCode206.py
+++ LeetCode206.py
@@ -1,3 +1,4 @@
+#Iterative
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
@@ -8,3 +9,17 @@ class Solution:
             prev = curr
             curr = nxt
         return prev
+
+#Recursvive
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
