992f6fafd51936efc3be0b4dfb2006d88f51c208
Nityanand7 <39838213+Nityanand7@users.noreply.github.com>
2024-01-22 18:11:07 -0600

    Create LeetCode48.py

---
+++ LeetCode48.py
@@ -0,0 +1,45 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                # save the topLeft
+                topLeft = matrix[top][l + i]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+
+            r -= 1
+            l += 1
+
+#alternate solution - Transpose and rotate the matrix
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        #transpose 
+        for row in range(len(matrix)):
+            for col in range(row,len(matrix)):
+                temp = matrix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][row] = temp
+
+        #reverse
+        for row in matrix:
+            row.reverse()
