1df5094a19c074b6b13fb6d627068c7da195235a
Nityanand7 <39838213+Nityanand7@users.noreply.github.com>
2024-02-01 09:42:50 -0600

    Update LeetCode104.py

--- LeetCode104.py
+++ LeetCode104.py
@@ -21,3 +21,17 @@ class Solution:
             
             level += 1
         return level
+    
+#Itervative    
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
